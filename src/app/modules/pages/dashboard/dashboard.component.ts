191b7aa52393f507f1c53cce344f7d21436b02c6
Milena <71612657+milenahas@users.noreply.github.com>
2024-04-15 17:14:06 -0300

    Adição de pomodoro

--- src/app/modules/pages/dashboard/dashboard.component.ts
+++ src/app/modules/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexStroke, ApexTooltip
 import { Cursos } from '../../shared/cursos';
 import { DashboardServiceService } from '../../shared/dashboard-service.service';
 declare var bootstrap: any;
+const moment = require('moment');
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -21,6 +22,14 @@ export type ChartOptions = {
 export class DashboardComponent implements OnInit {
 
   cursosEmAndamento: Cursos[] = [];
+  pomodoroMinutes = moment().minute(25).second(0).format('mm:ss');
+  maxMin = 25;
+
+  pomodoroOn: boolean = false;
+  
+  interval: any;
+  timeout: any;
+  elapsedTime: number = 0;
 
   @ViewChild("chart") chart: ChartComponent;
 
@@ -52,6 +61,35 @@ export class DashboardComponent implements OnInit {
         imagem: "assets/img/uiux.png"
       })
     }
+  }
+
+  comecarPomodoro(){
+    let initialValue = moment().minute(25).second(0);
+
+    this.interval = setInterval(() => {
+      initialValue.subtract(1, 'seconds').format('mm:ss');
+      this.pomodoroMinutes = initialValue.format('mm:ss');
+      this.pomodoroOn = true;
+    }, 1000);
+    
+    this.timeout = setInterval(() => {
+      const audio = new Audio();
+      audio.src = "../../../../assets/sounds/alarm-clock.mp3";
+      audio.load();
+      audio.play();
+      
+      clearInterval(this.interval);
+      clearInterval(this.timeout);
+      this.pomodoroOn = false;
+    }, this.maxMin * 60000);
+  }
+
+  cancelarPomodoro(){
+    this.pomodoroOn = false;
+    this.pomodoroMinutes = moment().minute(25).second(0).format('mm:ss');
+    
+    clearInterval(this.interval);
+    clearInterval(this.timeout); 
   }
 
 }
